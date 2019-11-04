7fc8e2ad586edcd2d0210c5ef202fdc7305908db
korends <koren.dlsantos@gmail.com>
2019-11-03 21:30:27 -0500

    Console log random number generated to match + appended number to html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,21 @@
-$(document).ready(function(){
-	$('#show-crystals').bind('click', function(){
-  	$(".crystal-images").toggle();
-  });
+$(document).ready(function () {
+
+    // HTML Text Variables
+
+    var wins = 0;
+    var losses = 0;
+    var computerNumber = (Math.floor(Math.random() * 120) + 19);
+    
+
+    //  Click button to show crystals
+    $('#show-crystals').bind('click', function () {
+        $(".crystal-images").toggle();
+        
+        // console log number to match
+        console.log(computerNumber);
+
+        // show the number to match on HTML
+        $("#numberguess-text").append("Number to match: " + computerNumber);
+        
+    });
 });
